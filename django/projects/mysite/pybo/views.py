3d7a1b92da5c74bc3a18aa2d194f422c0d90790f
hanjuren <juren528@gmail.com>
2022-10-04 16:58:28 +0900

    Django Paginator class

--- django/projects/mysite/pybo/views.py
+++ django/projects/mysite/pybo/views.py
@@ -2,20 +2,22 @@ from django.http import HttpResponseNotAllowed
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .form import QuestionForm, AnswerForm
-
+from django.core.paginator import Paginator
 from .models import Question
 
 
 # Create your views here.
 def index(request):
-    # 질문 리스트 출력
+    """
+    질문목록 조회
+    """
+    page = request.GET.get('page', '1')
     query = Question.objects.order_by('-created_at')
-    result = {
-        'total': len(query),
-        'result': query,
-    }
+    paginator = Paginator(query, 10)
+    page_obj = paginator.get_page(page)
+    context = {'result': page_obj}
 
-    return render(request, 'question_list.html', result)
+    return render(request, 'question_list.html', context)
 
 
 def detail(request, id_):
