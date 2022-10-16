90287639048021b44f0c23bd76fe6c43f6f8249a
hanjuren <juren528@gmail.com>
2022-10-16 14:51:00 +0900

    logout

--- django/projects/mysite/common/urls.py
+++ django/projects/mysite/common/urls.py
@@ -6,5 +6,7 @@ app_name = 'common'
 urlpatterns = [
     path('login', auth_views.LoginView.as_view(
         template_name='common/login.html'
-    ), name='login')
+    ), name='login'),
+
+    path('logout', auth_views.LogoutView.as_view(), name="logout"),
 ]
