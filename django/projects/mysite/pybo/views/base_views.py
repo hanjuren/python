2c866c70f2f220a97c7ec407b21da0a62aee51ed
hanjuren <juren528@gmail.com>
2022-10-19 21:21:20 +0900

    검색

--- django/projects/mysite/pybo/views/base_views.py
+++ django/projects/mysite/pybo/views/base_views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 
 from ..models import Question
 
@@ -9,10 +10,22 @@ def index(request):
     질문목록 조회
     """
     page = request.GET.get('page', '1')
+    kw = request.GET.get('kw', '')
+
     query = Question.objects.order_by('-created_at')
+
+    if kw:
+        query = query.filter(
+            Q(subject__icontains=kw) |
+            Q(content__icontains=kw) |
+            Q(answer__content__icontains=kw) |
+            Q(user__username__icontains=kw) |
+            Q(answer__user__username__icontains=kw)
+        ).distinct()
+
     paginator = Paginator(query, 10)
     page_obj = paginator.get_page(page)
-    context = {'result': page_obj}
+    context = {'result': page_obj, 'page': page, 'kw': kw}
 
     return render(request, 'pybo/question_list.html', context)
 
